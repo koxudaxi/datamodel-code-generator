5e3f5e04ea6009e8ae5024d69f6295cf543c62f3
Josh Bode <joshbode@fastmail.com>
2019-07-23 19:38:06 +1000

    add entrypoint

--- datamodel_code_generator/main.py
+++
@@ -1,5 +0,0 @@
-from datamodel_code_generator.model import BaseModel, DataModelField
-from datamodel_code_generator.parser.openapi import Parser
-
-if __name__ == '__main__':
-    Parser(BaseModel, DataModelField).parse()
