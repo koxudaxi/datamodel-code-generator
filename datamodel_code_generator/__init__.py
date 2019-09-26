da4f58260f045a30623de3fbad952d28b7b8cca9
Koudai Aono <koxudaxi@gmail.com>
2019-09-27 01:14:01 +0900

    add debug mode

--- datamodel_code_generator/__init__.py
+++ datamodel_code_generator/__init__.py
@@ -1,7 +1,50 @@
+import inspect
 from enum import Enum
+from typing import Type, TypeVar
+
+import pysnooper
+
+T = TypeVar('T')
+
+pysnooper.tracer.DISABLED = True
+
+
+def enable_debug_message() -> None:  # pragma: no cover
+    pysnooper.tracer.DISABLED = False
 
 
 class PythonVersion(Enum):
     PY_36 = '3.6'
     PY_37 = '3.7'
     PY_38 = '3.8'
+
+
+def snooper_to_methods(
+    output=None,
+    watch=(),
+    watch_explode=(),
+    depth=1,
+    prefix='',
+    overwrite=False,
+    thread_info=False,
+    custom_repr=(),
+    max_variable_length=100,
+) -> Type[T]:
+    def inner(cls: Type[T]) -> None:
+        methods = inspect.getmembers(cls, predicate=inspect.isfunction)
+        for name, method in methods:
+            snooper_method = pysnooper.snoop(
+                output,
+                watch,
+                watch_explode,
+                depth,
+                prefix,
+                overwrite,
+                thread_info,
+                custom_repr,
+                max_variable_length,
+            )(method)
+            setattr(cls, name, snooper_method)
+        return cls
+
+    return inner
