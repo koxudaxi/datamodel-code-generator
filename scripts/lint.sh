fc51d51bbcb84fff31601127eb50acc38b48c7d2
Koudai Aono <koxudaxi@gmail.com>
2019-10-09 20:47:57 +0900

    fix app name on script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,4 +3,4 @@ set -e
 
 black datamodel_code_generator tests --check --skip-string-normalization
 isort --recursive --check-only -w 88  --combine-as --thirdparty datamodel_code_generator datamodel_code_generator tests -m 3 -tc
-mypy pydataapi --disallow-untyped-defs --ignore-missing-imports
+mypy datamodel_code_generator --disallow-untyped-defs --ignore-missing-imports
