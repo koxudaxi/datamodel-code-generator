bed6dea29f5df512d105add193d75949f4c2d185
Josh Bode <joshbode@fastmail.com>
2020-04-01 14:18:17 +1100

    handle edge-case where models map to the __init__.py level of a module (#110)

--- tests/test_main_kr.py
+++ tests/test_main_kr.py
@@ -426,9 +426,12 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from .. import Id
+
 
 class Tea(BaseModel):
     flavour: Optional[str] = None
+    id: Optional[Id] = None
 
 
 class Cocoa(BaseModel):
