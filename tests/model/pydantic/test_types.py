e979f13a2419c219d2cb4637986368c551c4e37e
Martin Gonzalez Perna <31832018+mgonzalezperna@users.noreply.github.com>
2020-05-06 04:19:51 -0300

    Fix parse errors when property pattern is explicit. (#125)
    * Fix parse errors when property pattern is explicit.
    
    * Fix test for regex datatypes.
    
    The regex needs to be declared between quotes, otherwise it will be
    considered a normal string.
    
    * Remove redundant code.

--- tests/model/pydantic/test_types.py
+++ tests/model/pydantic/test_types.py
@@ -136,7 +136,7 @@ def test_get_data_float_type(types, params, data_type):
             DataType(
                 type='constr',
                 is_func=True,
-                kwargs={'regex': '^abc'},
+                kwargs={'regex': "'^abc'"},
                 imports_=[IMPORT_CONSTR],
             ),
         ),
