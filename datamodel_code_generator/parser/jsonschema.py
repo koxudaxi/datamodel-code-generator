2d9c30cff92c4149b53e4e8a195a02665c8b8e9c
yuyupopo <yuyupopo@snu.ac.kr>
2021-03-21 16:25:12 +0900

    Override target version on Literal datatypes

--- datamodel_code_generator/parser/jsonschema.py
+++ datamodel_code_generator/parser/jsonschema.py
@@ -521,7 +521,8 @@ class JsonSchemaParser(Parser):
                             field.additionalProperties
                         ):
                             additional_properties_type = self.data_type(
-                                literals=field.additionalProperties.enum
+                                literals=field.additionalProperties.enum,
+                                python_version=self.target_python_version,
                             )
                         else:
                             additional_properties_type = self.parse_enum(
@@ -567,7 +568,7 @@ class JsonSchemaParser(Parser):
                     field_type = self.data_type_manager.get_data_type(Types.object)
             elif field.enum:
                 if self.should_parse_enum_as_literal(field):
-                    field_type = self.data_type(literals=field.enum)
+                    field_type = self.data_type(literals=field.enum, python_version=self.target_python_version)
                 else:
                     field_type = self.parse_enum(field_name, field, [*path, field_name])
             else:
@@ -662,7 +663,7 @@ class JsonSchemaParser(Parser):
                 )
             elif item.enum:
                 if self.should_parse_enum_as_literal(item):
-                    return self.data_type(literals=item.enum)
+                    return self.data_type(literals=item.enum, python_version=self.target_python_version)
                 else:
                     return self.parse_enum(name, item, field_path, singular_name=True)
             elif item.is_array:
@@ -816,6 +817,7 @@ class JsonSchemaParser(Parser):
                     )
                     field_name = f'{prefix}_{enum_part}'
 
+            # TODO: remove this error once #380 gets resolved
             if not field_name:
                 raise Error(
                     f'Enum {name} contains empty string field.\n'
