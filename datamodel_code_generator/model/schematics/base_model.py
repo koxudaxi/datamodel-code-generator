5dddb74acf0846dc37acbfbe00c2b22614648403
Phil Leonowens <phillip.leonowens@pieinsurance.com>
2021-03-29 11:34:20 -0600

    Added recursion

--- datamodel_code_generator/model/schematics/base_model.py
+++ datamodel_code_generator/model/schematics/base_model.py
@@ -1,4 +1,5 @@
 from typing import ClassVar, Optional, Any, Tuple
+import re
 
 from datamodel_code_generator.imports import Import
 from datamodel_code_generator.model.base import DataModelFieldBase, DataModel
@@ -27,32 +28,102 @@ class SchematicsModelField(DataModelFieldBase):
     @property
     def model_name(self) -> Optional[str]:
         return self.data_type.reference.name if self.data_type.reference is not None \
-            else len(self.data_type.data_types) > 0 and self.data_type.data_types[-1].reference.name
+            else self.data_type.data_types[-1].reference.name if len(self.data_type.data_types) > 0 else None
 
     @property
     def is_required(self) -> bool:
         return self.nullable is not None and self.nullable or self.required
 
     @property
+    def snakecase_name(self) -> str:
+        if not self.name:
+            return 'NO_FIELD_SET'
+        name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', self.name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+    @property
     def is_list(self) -> bool:
         return self.data_type.is_list
 
     @property
-    def schematics_type(self) -> Optional[str]:
-        return self.model_name if self.is_model_type else self.data_type.full_name
+    def is_dict(self) -> bool:
+        return self.data_type.is_dict
+
+    @property
+    def is_nested_type(self) -> bool:
+        return self.is_list or self.is_dict
+
+    @property
+    def data_types(self) -> list:
+        return self.data_type.data_types
+
+    # @property
+    # def schematics_type(self) -> Optional[str]:
+    #     return self.model_name if self.is_model_type else self.data_type.full_name
 
     @property
     def inline_value(self) -> str:
-        if self.is_list and not self.is_model_type:
-            assembled_string = f"ListType({self.schematics_type}"
-        elif self.is_list and self.is_model_type:
-            assembled_string = f"ListType(ModelType({self.schematics_type})"
-        elif self.is_model_type:
-            assembled_string = f"ModelType({self.schematics_type}"
-        else:
-            assembled_string = f"{self.schematics_type}("
+        def _create_type_string(types_list: list) -> str:
+            """
+            Takes in list of types and formats them into schematics format
 
-        assembled_string += ")"
+            Returns of recursion of ['ListType', 'DictType', 'StringType'] will return for each subsequent call
+            [1] > 'StringType()'
+            [2] > 'DictType(StringType)'
+            [3] > 'ListType(DictType(StringType()), required=True)'
+
+
+            :param list[str] types_list: List of types, ex ['ListType', 'DictType', 'StringType']
+            :return: formatted string. ex 'ListType(DictType(StringType), serialized_name='blah', required=False)'
+            """
+
+            def recurse(sublist: list, is_top_level: bool = False):
+
+                outer = sublist[0]
+
+                # Is model type if reference attr exists
+                if outer.is_list:
+                    outer_type = 'ListType'
+                elif outer.is_dict:
+                    outer_type = 'DictType'
+                elif getattr(outer, 'reference'):
+                    outer_type = 'ModelType'
+                else:
+                    outer_type = outer.full_name
+                is_model_type = getattr(outer, 'reference')
+
+                if len(sublist) > 1:
+                    inner_type = recurse(sublist[1:])
+                else:
+                    inner_type = None
+                # Format kwargs for Type here. Will output in key=value format,
+                # i.e., {'required':True, 'serialized_name':'id'} > outputs > '(required=True, serialized_name=id)'
+                extra_kwargs = dict()
+                extra_kwargs['required'] = not self.data_type.is_optional
+                # Uncomment for less serialized names # if self.name and self.name != self.snakecase_name:
+                if self.name:
+
+                    extra_kwargs['serialized_name'] = self.name
+                if is_top_level:
+                    extra_kwarg_string = ((', ' if inner_type and extra_kwargs else '') + ", ".join(f"{key}={value}"
+                                          for key, value in extra_kwargs.items()))
+                else:
+                    extra_kwarg_string = ''
+
+                if is_model_type:
+                    # If this is the top level, give it kwarg string, if its nested, don't
+                    extra = f', {extra_kwarg_string}' if is_top_level else ''
+                    return f'ModelType({outer.reference.name}{extra})'
+
+                return f'{outer_type}({f"{inner_type}" if inner_type else ""}{extra_kwarg_string})'
+
+            return recurse(types_list, is_top_level=True)
+
+        # nested_type = 'ListType' if self.is_list else 'DictType'
+        # Else recurse through data_types
+        data_types = [self.data_type] + self.data_type.data_types
+        assembled_string = _create_type_string(data_types)
+
         return assembled_string
 
 
