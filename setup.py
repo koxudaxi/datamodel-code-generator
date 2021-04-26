1946a9e164c0c3b4798df77a7ebeb15aa2f95469
Mark Rogers <mark.rogers@pieinsurance.com>
2021-04-26 10:18:16 -0600

    Add version declaration for consumers to use to pin dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from pathlib import Path
 from setuptools import setup
 from setuptools.config import read_configuration
 
+# This Version number is formatted such that the latest version of the Upstream that is integrated precedes the
+# underscore, and Pie's version succeeds the underscore.
+VERSION = '0.9.2_1.1.0'
+
 config = read_configuration(Path(__file__).parent.joinpath('setup.cfg'))
 
 extras_require = {
@@ -15,4 +19,7 @@ extras_require = {
 extras_require['all'] = [*extras_require.values()]
 use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
 
-setup(extras_require=extras_require, use_scm_version=use_scm_version)
+setup(version=VERSION,
+      extras_require=extras_require,
+      use_scm_version=use_scm_version
+)
