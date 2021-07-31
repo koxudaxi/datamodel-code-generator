a4be66fda580bdb6cc104469d424a9b1057349c6
Koudai Aono <koxudaxi@gmail.com>
2021-07-31 18:32:45 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@
 
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import __version__, setup
 from setuptools.config import read_configuration
+
+if int(__version__.split(".")[0]) < 41:
+    raise RuntimeError("setuptools >= 41 required to build")
 
 config = read_configuration(Path(__file__).parent.joinpath('setup.cfg'))
 
@@ -14,4 +17,4 @@ extras_require = {
 extras_require['all'] = [*extras_require.values()]
 use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
 
-setup(extras_require=extras_require, use_scm_version=use_scm_version)
+setup(setup_requires=["setuptools_scm >= 2"], extras_require=extras_require, use_scm_version=use_scm_version)
