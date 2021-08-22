5bdf3cf81172f9925e5e9ab80837adba515b8b10
Koudai Aono <koxudaxi@gmail.com>
2021-08-23 00:04:25 +0900

    fix unexpected changes

--- datamodel_code_generator/parser/jsonschema.py
+++ datamodel_code_generator/parser/jsonschema.py
@@ -307,8 +307,8 @@ class JsonSchemaParser(Parser):
         custom_class_name_generator: Optional[Callable[[str], str]] = None,
         field_extra_keys: Optional[Set[str]] = None,
         field_include_all_keys: bool = False,
-        wrap_string_literal: bool = False,
-        use_title_as_name: Optional[bool] = None,
+        wrap_string_literal: Optional[bool] = False,
+        use_title_as_name: bool = None,
     ):
         super().__init__(
             source=source,
