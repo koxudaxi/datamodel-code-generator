f16d58fb47ccce71d0e881f6e8a02d7e8a41595f
Koudai Aono <koxudaxi@gmail.com>
2022-11-09 02:19:55 +0900

    Show help when no input

--- datamodel_code_generator/__main__.py
+++ datamodel_code_generator/__main__.py
@@ -529,6 +529,10 @@ def main(args: Optional[Sequence[str]] = None) -> Exit:
         print(e.message, file=sys.stderr)
         return Exit.ERROR
 
+    if not config.input and not config.url and sys.stdin.isatty():
+        arg_parser.print_help()
+        return Exit.OK
+
     if not is_supported_in_black(config.target_python_version):  # pragma: no cover
         print(
             f"Installed black doesn't support Python version {config.target_python_version.value}.\n"  # type: ignore
