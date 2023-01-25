8c4c76d9307ca17c3a6b93ee5eb0200608d1b7b3
Koudai Aono <koxudaxi@gmail.com>
2023-01-25 22:48:23 +0900

    Remove duplicate model

--- tests/data/expected/main/main_openapi_override_required_all_of/output.py
+++ tests/data/expected/main/main_openapi_override_required_all_of/output.py
@@ -15,23 +15,18 @@ class Type(Enum):
     my_second_object = 'my_second_object'
 
 
-class TypeModel(Enum):
-    my_first_object = 'my_first_object'
-    my_second_object = 'my_second_object'
-
-
 class ObjectBase(BaseModel):
     name: Optional[str] = Field(None, description='Name of the object')
     type: Optional[Type] = Field(None, description='Object type')
     rank: Optional[Union[int, float]] = Field(None, description='User rank')
-    allIn: Optional[Union[TypeModel, str, Union[int, float]]] = None
+    allIn: Optional[Union[Type, str, Union[int, float]]] = None
 
 
 class CreateObjectRequest(ObjectBase):
     name: str = Field(..., description='Name of the object')
     type: Type = Field(..., description='Object type')
     rank: Union[int, float] = Field(..., description='User rank')
-    allIn: Union[TypeModel, str, Union[int, float]]
+    allIn: Union[Type, str, Union[int, float]]
 
 
 class UpdateObjectRequest(ObjectBase):
