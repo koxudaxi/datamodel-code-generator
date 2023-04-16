03eeddbabf9259dd083a4055809ba62d26178e6c
KotlinIsland <65446343+KotlinIsland@users.noreply.github.com>
2023-04-17 03:16:27 +1000

    (🎁) infer data vs schema when `--input-file-type` is auto (#1249)
    * infer data vs schema when `--input-file-type` is auto
    
    * Improve is_schema conditions
    
    * Fix coverage
    
    * Fix is_schema
    
    * Improve coverage
    
    * Improve coverage
    
    * Improve coverage
    
    ---------
    
    Co-authored-by: KotlinIsland <kotlinisland@users.noreply.github.com>
    Co-authored-by: Koudai Aono <koxudaxi@gmail.com>

--- datamodel_code_generator/__init__.py
+++ datamodel_code_generator/__init__.py
@@ -168,6 +168,37 @@ def is_openapi(text: str) -> bool:
     return 'openapi' in load_yaml(text)
 
 
+JSON_SCHEMA_URLS: Tuple[str, ...] = (
+    'http://json-schema.org/',
+    'https://json-schema.org/',
+)
+
+
+def is_schema(text: str) -> bool:
+    data = load_yaml(text)
+    if not isinstance(data, dict):
+        return False
+    schema = data.get('$schema')
+    if isinstance(schema, str) and any(
+        schema.startswith(u) for u in JSON_SCHEMA_URLS
+    ):  # pragma: no cover
+        return True
+    if isinstance(data.get('type'), str):
+        return True
+    if any(
+        isinstance(data.get(o), list)
+        for o in (
+            'allOf',
+            'anyOf',
+            'oneOf',
+        )
+    ):
+        return True
+    if isinstance(data.get('properties'), dict):
+        return True
+    return False
+
+
 class InputFileType(Enum):
     Auto = 'auto'
     OpenAPI = 'openapi'
@@ -304,11 +335,8 @@ def generate(
                 if isinstance(input_, Path)
                 else input_text
             )
-            input_file_type = (
-                InputFileType.OpenAPI
-                if is_openapi(input_text_)  # type: ignore
-                else InputFileType.JsonSchema
-            )
+            assert isinstance(input_text_, str)
+            input_file_type = infer_input_type(input_text_)
             print(
                 inferred_message.format(input_file_type.value),
                 file=sys.stderr,
@@ -481,6 +509,14 @@ def generate(
 
         if file is not None:
             file.close()
+
+
+def infer_input_type(text: str) -> InputFileType:
+    if is_openapi(text):
+        return InputFileType.OpenAPI
+    elif is_schema(text):
+        return InputFileType.JsonSchema
+    return InputFileType.Json
 
 
 inferred_message = (
---
+++ tests/test_infer_input_type.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from datamodel_code_generator import InputFileType, infer_input_type
+
+DATA_PATH: Path = Path(__file__).parent / 'data'
+
+
+def test_infer_input_type():
+    def assert_infer_input_type(file: Path, raw_data_type: InputFileType) -> None:
+        __tracebackhide__ = True
+        if file.is_dir():
+            return
+        if file.suffix not in ('.yaml', '.json'):
+            return
+        result = infer_input_type(file.read_text())
+        assert result == raw_data_type, f'{file} was the wrong type!'
+
+    for file in (DATA_PATH / 'json').rglob('*'):
+        if str(file).endswith('broken.json'):
+            continue
+        assert_infer_input_type(file, InputFileType.Json)
+    for file in (DATA_PATH / 'jsonschema').rglob('*'):
+        if str(file).endswith(('external_child.json', 'external_child.yaml')):
+            continue
+        if 'reference_same_hierarchy_directory' in str(file):
+            continue
+        assert_infer_input_type(file, InputFileType.JsonSchema)
+    for file in (DATA_PATH / 'openapi').rglob('*'):
+        if str(file).endswith(
+            (
+                'aliases.json',
+                'extra_data.json',
+                'invalid.yaml',
+                'list.json',
+                'empty_data.json',
+                'root_model.yaml',
+                'json_pointer.yaml',
+                'const.json',
+            )
+        ):
+            continue
+
+        if str(file).endswith('not.json'):
+            assert_infer_input_type(file, InputFileType.Json)
+            continue
+        assert_infer_input_type(file, InputFileType.OpenAPI)
