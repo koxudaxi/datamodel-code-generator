7d7ae014258b939c8a1bb12fbc1aa553c02f9433
Koudai Aono <koxudaxi@gmail.com>
2023-06-06 01:34:51 +0900

    Add unittest

--- tests/test_main.py
+++ tests/test_main.py
@@ -5595,3 +5595,29 @@ def test_main_typed_dict_nullable_strict_nullable():
 
     with pytest.raises(SystemExit):
         main()
+
+
+@freeze_time('2019-07-26')
+def test_main_custom_file_header_path():
+    with TemporaryDirectory() as output_dir:
+        output_file: Path = Path(output_dir) / 'output.py'
+        return_code: Exit = main(
+            [
+                '--input',
+                str(OPEN_API_DATA_PATH / 'api.yaml'),
+                '--output',
+                str(output_file),
+                '--custom-file-header-path',
+                str(DATA_PATH / 'custom_file_header.txt'),
+            ]
+        )
+        assert return_code == Exit.OK
+        assert (
+            output_file.read_text()
+            == (
+                EXPECTED_MAIN_PATH / 'main_custom_file_header' / 'output.py'
+            ).read_text()
+        )
+
+    with pytest.raises(SystemExit):
+        main()
