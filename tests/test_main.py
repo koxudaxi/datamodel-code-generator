95df46ea40910611f184f686503f4cfdcb263ab8
Fokko Driesprong <fokko@apache.org>
2023-06-05 18:57:35 +0200

    Fix the custom file header (#1346)
    * Fix the custom file header
    
    * Add the ability to read the header from a file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add unittest
    
    * Add unittest
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koudai Aono <koxudaxi@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -5620,3 +5620,54 @@ def test_main_typed_dict_nullable_strict_nullable():
 
     with pytest.raises(SystemExit):
         main()
+
+
+@freeze_time('2019-07-26')
+def test_main_custom_file_header_path():
+    with TemporaryDirectory() as output_dir:
+        output_file: Path = Path(output_dir) / 'output.py'
+        return_code: Exit = main(
+            [
+                '--input',
+                str(OPEN_API_DATA_PATH / 'api.yaml'),
+                '--output',
+                str(output_file),
+                '--custom-file-header-path',
+                str(DATA_PATH / 'custom_file_header.txt'),
+            ]
+        )
+        assert return_code == Exit.OK
+        assert (
+            output_file.read_text()
+            == (
+                EXPECTED_MAIN_PATH / 'main_custom_file_header' / 'output.py'
+            ).read_text()
+        )
+
+    with pytest.raises(SystemExit):
+        main()
+
+
+@freeze_time('2019-07-26')
+def test_main_custom_file_header_duplicate_options(capsys):
+    with TemporaryDirectory() as output_dir:
+        output_file: Path = Path(output_dir) / 'output.py'
+        return_code: Exit = main(
+            [
+                '--input',
+                str(OPEN_API_DATA_PATH / 'api.yaml'),
+                '--output',
+                str(output_file),
+                '--custom-file-header-path',
+                str(DATA_PATH / 'custom_file_header.txt'),
+                '--custom-file-header',
+                'abc',
+            ]
+        )
+
+        captured = capsys.readouterr()
+        assert return_code == Exit.ERROR
+        assert (
+            captured.err
+            == '`--custom_file_header_path` can not be used with `--custom_file_header`.\n'
+        )
