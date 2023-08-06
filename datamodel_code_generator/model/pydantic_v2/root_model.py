e2b2f13f12c18dd5b54416b7a5bf215bf11e1617
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-08-06 19:19:30 +0200

    Add type annotation

--- datamodel_code_generator/model/pydantic_v2/root_model.py
+++ datamodel_code_generator/model/pydantic_v2/root_model.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import ClassVar
+from typing import ClassVar, TypedDict
 
 from datamodel_code_generator.model.pydantic_v2.base_model import BaseModel
 
@@ -11,8 +11,8 @@ class RootModel(BaseModel):
 
     def __init__(
         self,
-        **kwargs,
-    ):
+        **kwargs: TypedDict,
+    ) -> None:
         # Remove custom_base_class for Pydantic V2 models; behaviour is different from Pydantic V1 as it will not
         # be treated as a root model. custom_base_class cannot both implement BaseModel and RootModel!
         kwargs.pop('custom_base_class')
