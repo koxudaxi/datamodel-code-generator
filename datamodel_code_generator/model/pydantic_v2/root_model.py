2f229dad41c6ac2d675355bd1dbf1cd982b0d2c2
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-08-06 18:22:39 +0200

    Fix Pydantic V2 RootModel inheritance
    In Pydantic V2, root models are defined by inheriting RootModel class and defining the root field.

--- datamodel_code_generator/model/pydantic_v2/root_model.py
+++ datamodel_code_generator/model/pydantic_v2/root_model.py
@@ -8,3 +8,13 @@ from datamodel_code_generator.model.pydantic_v2.base_model import BaseModel
 class RootModel(BaseModel):
     TEMPLATE_FILE_PATH: ClassVar[str] = 'pydantic_v2/RootModel.jinja2'
     BASE_CLASS: ClassVar[str] = 'pydantic.RootModel'
+
+    def __init__(
+            self,
+            **kwargs,
+    ):
+        # Remove custom_base_class for Pydantic V2 models; behaviour is different from Pydantic V1 as it will not
+        # be treated as a root model. custom_base_class cannot both implement BaseModel and RootModel!
+        kwargs.pop("custom_base_class")
+
+        super().__init__(**kwargs)
