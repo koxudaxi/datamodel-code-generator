a721166e9fa2da5b8a52345db07b4e367d72b3c2
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-08-08 18:01:58 +0200

    Fix Pydantic V2 RootModel inheritance (#1477)
    * Fix Pydantic V2 RootModel inheritance
    
    In Pydantic V2, root models are defined by inheriting RootModel class and defining the root field.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add type annotation
    
    * Include test cases
    
    * Changed TypedDict to Any
    
    TypedDict is not available on pre-3.8, so changing this to Any to avoid issues...
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/model/pydantic_v2/test_root_model.py
@@ -0,0 +1,67 @@
+from datamodel_code_generator.model import DataModelFieldBase
+from datamodel_code_generator.model.pydantic_v2.root_model import RootModel
+from datamodel_code_generator.reference import Reference
+from datamodel_code_generator.types import DataType
+
+
+def test_root_model():
+    root_model = RootModel(
+        fields=[
+            DataModelFieldBase(
+                name='a',
+                data_type=DataType(type='str'),
+                default='abc',
+                required=False,
+            )
+        ],
+        reference=Reference(name='TestRootModel', path='test_root_model'),
+    )
+
+    assert root_model.name == 'TestRootModel'
+    assert root_model.fields == [
+        DataModelFieldBase(
+            name='a',
+            data_type=DataType(type='str'),
+            default='abc',
+            required=False,
+            parent=root_model,
+        )
+    ]
+
+    assert root_model.base_class == 'RootModel'
+    assert root_model.custom_base_class is None
+    assert root_model.render() == (
+        'class TestRootModel(RootModel):\n' "    root: Optional[str] = 'abc'"
+    )
+
+
+def test_root_model_custom_base_class():
+    root_model = RootModel(
+        custom_base_class='test.Test',
+        fields=[
+            DataModelFieldBase(
+                name='a',
+                data_type=DataType(type='str'),
+                default='abc',
+                required=False,
+            )
+        ],
+        reference=Reference(name='TestRootModel', path='test_root_model'),
+    )
+
+    assert root_model.name == 'TestRootModel'
+    assert root_model.fields == [
+        DataModelFieldBase(
+            name='a',
+            data_type=DataType(type='str'),
+            default='abc',
+            required=False,
+            parent=root_model,
+        )
+    ]
+
+    assert root_model.base_class == 'RootModel'
+    assert root_model.custom_base_class is None
+    assert root_model.render() == (
+        'class TestRootModel(RootModel):\n' "    root: Optional[str] = 'abc'"
+    )
