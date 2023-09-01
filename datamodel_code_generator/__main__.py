d6fd3ae5128a25b86b6726630359cc216631c951
Toby Woodwark <toby.woodwark@virginmediao2.co.uk>
2023-09-01 14:28:53 +0100

    Amend the use_default_kwarg default to False
    It is a CLI flag and should be False when unset.

--- datamodel_code_generator/__main__.py
+++ datamodel_code_generator/__main__.py
@@ -621,7 +621,7 @@ class Config(BaseModel):
     use_standard_collections: bool = False
     use_schema_description: bool = False
     use_field_description: bool = False
-    use_default_kwarg: bool = True
+    use_default_kwarg: bool = False
     reuse_model: bool = False
     encoding: str = DEFAULT_ENCODING
     enum_field_as_literal: Optional[LiteralType] = None
