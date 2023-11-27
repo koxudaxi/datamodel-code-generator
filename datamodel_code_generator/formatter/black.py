e467f8b4fac55313a9db1d013522f4da165140f7
denisart <artyushinden@gmail.com>
2023-11-27 14:06:31 +0300

    Run poetry run scripts/format.sh

--- datamodel_code_generator/formatter/black.py
+++ datamodel_code_generator/formatter/black.py
@@ -90,7 +90,9 @@ class BlackCodeFormatter(BaseCodeFormatter):
         else:
             self.black_mode = black.FileMode(
                 target_versions={
-                    BLACK_PYTHON_VERSION[formatter_kwargs.get('target-version', PythonVersion.PY_37)]
+                    BLACK_PYTHON_VERSION[
+                        formatter_kwargs.get('target-version', PythonVersion.PY_37)
+                    ]
                 },
                 line_length=formatter_kwargs.get(
                     'line-length', black.DEFAULT_LINE_LENGTH
