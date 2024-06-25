14b6e6329e2f748f65038b7b4b35802161936b10
Luca Knaack <luca.knaack@web-computing.de>
2024-06-25 17:15:00 +0200

    patch pydantic v1.10.9 conflict with py3.12

---
+++ datamodel_code_generator/pydantic_patch.py
@@ -0,0 +1,14 @@
+import pydantic.typing
+
+
+def patched_evaluate_forwardref(forward_ref, globalns, localns=None):
+    try:
+        return forward_ref._evaluate(globalns, localns or None, set())
+    except TypeError:
+        # Fallback for Python 3.12 compatibility
+        return forward_ref._evaluate(
+            globalns, localns or None, set(), recursive_guard=set()
+        )
+
+
+pydantic.typing.evaluate_forwardref = patched_evaluate_forwardref
