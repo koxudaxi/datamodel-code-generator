1029b18d339193760afc3b70922e608284288c86
Koudai Aono <koxudaxi@gmail.com>
2024-08-08 15:38:56 +0900

    Revert "add command help update script (#2062)"
    This reverts commit 2d6275cc86d5b6925e44dc80a94d317de44cdef6.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,5 @@ set -e
 
 ruff check datamodel_code_generator tests
 ruff format --check datamodel_code_generator tests
-python scripts/update_command_help_on_markdown.py --validate
 
 mypy datamodel_code_generator
