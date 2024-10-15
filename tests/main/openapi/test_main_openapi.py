8cd56d4983ebc336b58a1f4b84dde4fbc0cacf97
Koudai Aono <koxudaxi@gmail.com>
2024-10-16 03:18:37 +0900

    fix unittest

--- tests/main/openapi/test_main_openapi.py
+++ tests/main/openapi/test_main_openapi.py
@@ -2890,6 +2890,10 @@ def test_main_openapi_discriminator_one_literal_as_default_dataclass():
 
 
 @freeze_time('2019-07-26')
+@pytest.mark.skipif(
+    black.__version__.split('.')[0] == '19',
+    reason="Installed black doesn't support the old style",
+)
 def test_main_openapi_keyword_only_dataclass():
     with TemporaryDirectory() as output_dir:
         output_file: Path = Path(output_dir) / 'output.py'
