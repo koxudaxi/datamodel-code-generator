e1824b200824a739d1d40583c681beb2c03707ec
mueslo <johannesfalke@googlemail.com>
2025-07-05 16:59:08 +0200

    fix: revert default date-time behaviour
    set openapi and jsonschema default target datetime to AwareDatetime to conform to specification

--- src/datamodel_code_generator/parser/base.py
+++ src/datamodel_code_generator/parser/base.py
@@ -376,7 +376,7 @@ class Parser(ABC):
         treat_dot_as_module: bool = False,
         use_exact_imports: bool = False,
         default_field_extras: dict[str, Any] | None = None,
-        target_datetime_class: DatetimeClassType | None = DatetimeClassType.Datetime,
+        target_datetime_class: DatetimeClassType | None = None,
         keyword_only: bool = False,
         frozen_dataclasses: bool = False,
         no_alias: bool = False,
