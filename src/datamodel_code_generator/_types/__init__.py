3f06fa4b0c18ab442d79347c4b5031ac3f531c4f
Koudai Aono <koxudaxi@gmail.com>
2026-01-01 05:26:59 +0900

    Consolidate ParserConfig TypedDict profiles with inheritance preservation (#2883)

--- src/datamodel_code_generator/_types/__init__.py
+++ src/datamodel_code_generator/_types/__init__.py
@@ -3,11 +3,13 @@
 from __future__ import annotations
 
 from datamodel_code_generator._types.generate_config_dict import GenerateConfigDict
-from datamodel_code_generator._types.graphql_parser_config_dict import GraphQLParserConfigDict
-from datamodel_code_generator._types.jsonschema_parser_config_dict import JSONSchemaParserConfigDict
-from datamodel_code_generator._types.openapi_parser_config_dict import OpenAPIParserConfigDict
 from datamodel_code_generator._types.parse_config_dict import ParseConfigDict
-from datamodel_code_generator._types.parser_config_dict import ParserConfigDict
+from datamodel_code_generator._types.parser_config_dicts import (
+    GraphQLParserConfig as GraphQLParserConfigDict,
+    JSONSchemaParserConfig as JSONSchemaParserConfigDict,
+    OpenAPIParserConfig as OpenAPIParserConfigDict,
+    ParserConfig as ParserConfigDict,
+)
 
 __all__ = [
     "GenerateConfigDict",
--- src/datamodel_code_generator/_types/parser_config_dicts.py
+++ src/datamodel_code_generator/_types/parser_config_dicts.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypedDict
+from typing import TYPE_CHECKING, Any, TypeAlias, TypedDict
 
 from typing_extensions import NotRequired
 
@@ -19,6 +19,7 @@ if TYPE_CHECKING:
         DataclassArguments,
         FieldTypeCollisionStrategy,
         NamingStrategy,
+        OpenAPIScope,
         ReadOnlyWriteOnlyModelType,
         ReuseScope,
         StrictTypes,
@@ -144,5 +145,19 @@ class ParserConfig(TypedDict):
     target_pydantic_version: NotRequired[TargetPydanticVersion | None]
 
 
-class JSONSchemaParserConfigDict(ParserConfig):
+class GraphQLParserConfig(ParserConfig):
+    data_model_scalar_type: NotRequired[type[DataModel]]
+    data_model_union_type: NotRequired[type[DataModel]]
+
+
+class JSONSchemaParserConfig(ParserConfig):
     pass
+
+
+class OpenAPIParserConfig(JSONSchemaParserConfig):
+    openapi_scopes: NotRequired[list[OpenAPIScope] | None]
+    include_path_parameters: NotRequired[bool]
+    use_status_code_in_response_name: NotRequired[bool]
+
+
+Model: TypeAlias = ParserConfig | GraphQLParserConfig | JSONSchemaParserConfig | OpenAPIParserConfig
