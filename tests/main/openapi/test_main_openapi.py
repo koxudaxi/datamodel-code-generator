670f35d3a799f2a54a9c35c5ca2ed0ad9d3ad7d9
Kevin Paulson <kevin.paulson@mindbridge.ai>
2026-04-16 12:25:39 -0400

    Code format mostly

--- tests/main/openapi/test_main_openapi.py
+++ tests/main/openapi/test_main_openapi.py
@@ -5317,11 +5317,14 @@ def test_main_reuse_model_with_type_alias(output_file: Path) -> None:
         ],
     )
 
+
 @pytest.mark.timeout(30)
 def test_main_to_be_renamed_later(output_file: Path) -> None:
-     run_main_and_assert(
+    """To be updated later."""
+    run_main_and_assert(
         input_path=OPEN_API_DATA_PATH / "to_be_renamed_later.json",
         output_path=output_file,
         input_file_type="openapi",
         assert_func=assert_file_content,
+        extra_args=["--disable-timestamp"],
     )
