5f8b6ea770ac3aff9d195c797c7e8db01fe1bd1a
Koudai Aono <koxudaxi@gmail.com>
2026-06-04 07:57:29 +0000

    Cover GraphQL null input defaults

--- tests/data/expected/main/graphql/pydantic_v2_empty_list_default.py
+++ tests/data/expected/main/graphql/pydantic_v2_empty_list_default.py
@@ -31,4 +31,5 @@ class PodSpec(BaseModel):
     container_or_none_list_or_none: list[Container | None] | None = Field(
         [], validate_default=True
     )
+    nullable_default_null: String | None = None
     typename__: Literal['PodSpec'] | None = Field('PodSpec', alias='__typename')
